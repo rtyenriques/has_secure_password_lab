477d23dcc225f62b49f5e7fa4d1e1887ce1082e4
= <rtyenriques@gmail.com>
2021-10-08 12:09:28 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'users#new', as: 'user'
+  post '/', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/:user', to: 'users#show', as: 'homepage'
 end
